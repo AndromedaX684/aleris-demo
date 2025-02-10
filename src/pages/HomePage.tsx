97b8d5cd94f1e22bd9a8a2ed588bdbb992a209f4
AndromedaX684 <andromedax684@gmail.com>
2025-02-10 18:02:54 +0900

    more margin on the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,11 +2,11 @@ import NavCards from "@/features/homePage/NavCards";
 
 export default function HomePage() {
 	return (
-		<div className="flex flex-col my-16">
+		<div className="flex flex-col my-32 overflow-auto">
 			<div className="text-center mb-12">
 				<h1 className="text-4xl font-bold mb-2">Demonstrasjon av Apper</h1>
 				<p className="text-lg text-gray-600">
-					Dette er en demo for å vise frem app-ideer for Aleris
+					Dette er en demo for å vise frem app-ideer til Aleris
 				</p>
 			</div>
 			<NavCards />
