c57c46febb73537d3601f6d7480cdad93ea7a061
AndromedaX684 <andromedax684@gmail.com>
2025-02-10 17:59:30 +0900

    made header fixed

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -18,7 +18,7 @@ export default function SideBar() {
 		<SidebarProvider>
 			<HomeSidebar />
 			<SidebarInset>
-				<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 sticky top-0 bg-inherit">
+				<header className="flex w-full h-16 shrink-0 items-center gap-2 border-b px-4 fixed top-0 bg-inherit">
 					<SidebarTrigger className="-ml-1" />
 					<Separator orientation="vertical" className="mr-2 h-4" />
 					<Breadcrumb>
