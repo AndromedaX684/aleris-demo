238e58d98a557574555535c5ac269addce03fa9a
AndromedaX684 <andromedax684@gmail.com>
2025-02-12 16:50:59 +0900

    removed margin bottom home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import NavCards from "@/features/homePage/NavCards";
 export default function HomePage() {
 	return (
 		<div className="flex flex-col my-32 overflow-auto">
-			<div className="text-center mb-12">
+			<div className="text-center">
 				<h1 className="text-4xl font-bold mb-2">Demonstrasjon av Apper</h1>
 				<p className="text-lg text-gray-600">
 					Dette er en demo for å vise frem app-ideer til Aleris
