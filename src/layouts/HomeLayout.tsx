bf1198bdd6252cffcba6bcaab75e5a2d09c3ca98
AndromedaX684 <andromedax684@gmail.com>
2025-02-12 14:37:14 +0900

    testing fixed header on home page

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -19,7 +19,7 @@ export default function SideBar() {
 		<SidebarProvider>
 			<HomeSidebar />
 			<SidebarInset>
-				<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 sticky top-0 z-10 bg-inherit">
+				<header className="bg-background fixed inset-x-0 top-0 isolate z-10 flex shrink-0 items-center gap-2 border-b">
 					<SidebarTrigger className="-ml-1" />
 					<Separator orientation="vertical" className="mr-2 h-4" />
 					<Breadcrumb>
